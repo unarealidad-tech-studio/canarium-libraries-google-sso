eb6ef391458dc5860e49214c34bd827ae9eb0386
Errol A. Zuñiga Jr <errol.zuniga@unarealidad.com>
2016-10-22 13:50:57 +0800

    debug

--- src/GoogleSSO/View/Helper/CreateAuthUrl.php
+++ src/GoogleSSO/View/Helper/CreateAuthUrl.php
@@ -11,6 +11,8 @@ class CreateAuthUrl extends AbstractHelper
 
         $auth_url = str_replace('&approval_prompt=auto', '', $auth_url);
 
+        var_dump($auth_url);exit;
+
         return $auth_url;
     }
 }
